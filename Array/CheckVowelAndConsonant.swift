288f9ed09df1110a27f08e9dfbb61cce8e572c8b
varu0908 <131972865+varu0908@users.noreply.github.com>
2023-08-01 18:16:07 +0530

    Create CheckVowelAndConsonant.swift

---
+++ Array/CheckVowelAndConsonant.swift
@@ -0,0 +1,28 @@
+// Check Vowels and Consonant
+func checkVowelAndConsonant(_ char : String) -> String {
+    switch char {
+    case "A" :
+        return ("\(char) is Vowel")
+    case "E" :
+        return("\(char) is Vowel")
+    case "I" :
+        return("\(char) is Vowel")
+    case "O" :
+        return("\(char) is Vowel")
+    case "U" :
+        return("\(char) is Vowel")
+    case "a" :
+        return("\(char) is Vowel")
+    case "e" :
+        return("\(char) is Vowel")
+    case "i" :
+        return("\(char) is Vowel")
+    case "o" :
+        return("\(char) is Vowel")
+    case "u" :
+        return("\(char) is Vowel")
+    default:
+        return ("\(char) is Consonent")
+    }
+}
+checkVowelAndConsonant("B")
