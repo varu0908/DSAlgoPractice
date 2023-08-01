1e9d53cf22900c7ec6d6fca67e5827112b372962
varu0908 <131972865+varu0908@users.noreply.github.com>
2023-08-01 17:39:00 +0530

    Update BubbleSort.swift

--- Sorting/BubbleSort.swift
+++ Sorting/BubbleSort.swift
@@ -1,1 +1,19 @@
+var arr = [1,5,3,4,2,6]
+func bubbleSortForrPreArray(ar : inout [Int]) {
+    var i = 0
+    while(i < ar.count) {
+        var j = 0
+        while(j < ar.count - i - 1) {
+            if ar[j] > ar[j+1] {
+                var temp = ar[j]
+                ar[j] = ar[j+1]
+                ar[j+1] = temp
+            }
+            j += 1
+        }
+        i += 1
+    }
+}
+bubbleSortForrPreArray(ar: &arr)
 
+// O/P -> [1,2,3,4,5,6]
