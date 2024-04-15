c11da7a79254d6984ea6ded80afe0c042587a7b0
varu0908 <131972865+varu0908@users.noreply.github.com>
2024-04-15 12:05:23 +0530

    SearchInBinaryTree.swift

---
+++ Tree/SearchInBinaryTree.swift
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        if root == nil {
+            return nil
+        }
+        if root!.val == val {
+            return root
+        }
+        var leftAns = searchBST(root?.left, val)
+        if leftAns != nil {
+            return leftAns
+        }
+        var rightAns = searchBST(root?.right, val)
+        return rightAns
+    }
+}
